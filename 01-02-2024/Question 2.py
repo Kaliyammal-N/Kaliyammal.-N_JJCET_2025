bbe321c5cb69cd0a61c3ca5e4444f8a545dc2100
Kaliyammal-N <146991299+Kaliyammal-N@users.noreply.github.com>
2024-02-03 12:41:13 +0530

    Update Question 2.py

--- 01-02-2024/Question 2.py
+++ 01-02-2024/Question 2.py
@@ -4,39 +4,13 @@ An Anagram is a word or phrase formed by rearranging the letters of a differen
 Example 1:
 Input: s = "anagram", t = "nagaram" Output: true 
 Example 2:
-Input: s = "rat", t = "car" Output: false
+Input: s = "rat" , t = "car"
+output: false
+
 class Solution:
 
-    def productExceptSelf(self, nums: List[int]) -> List[int]:
+    def isAnagram(self, s: str, t: str) -> bool:
 
-        n = len(nums)
+        return sorted(s) == sorted(t)
 
-        left_products, right_products, result = [1] * n, [1] * n, [1] * n
 
-        left_product, right_product = 1, 1
-
-        for i in range(n):
-
-            left_products[i] = left_product
-
-            right_products[n - 1 - i] = right_product
-
-            left_product *= nums[i]
-
-            right_product *= nums[n - 1 - i]
-
-        for i in range(n):
-
-            result[i] = left_products[i] * right_products[i]
-
-        return result
-
-# Example usage:
-
-nums = [1, 2, 3, 4]
-
-solution = Solution()
-
-result = solution.productExceptSelf(nums)
-
-print(result)
